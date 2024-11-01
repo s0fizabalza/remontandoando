773c01ceb645397ec2d6d6baf5048b9e280f6d00
s0fizabalza <sofiromero108@gmail.com>
2024-11-01 10:15:50 -0300

    me adelanteé

--- ejer12/ejer12.cs
+++ ejer12/ejer12.cs
@@ -1,7 +1,8 @@
 public class CuentaBancaria
 {
     public Persona Titular { get; set; }
-    public decimal Saldo { get; private set; } // El saldo solo se puede modificar desde la clase
+    public decimal Saldo { get; private set; } //modifica con la clase
+    
 
     public CuentaBancaria(Persona titular, decimal saldoInicial)
     {
@@ -9,31 +10,4 @@ public class CuentaBancaria
         Saldo = saldoInicial;
     }
 
-    public void IngresarDinero(decimal cantidad)
-    {
-        if (cantidad > 0)
-        {
-            Saldo += cantidad;
-            Console.WriteLine($"Se han ingresado ${cantidad} a la cuenta de {Titular.Nombre}");
-        }
-        else
-        {
-            Console.WriteLine("La cantidad a ingresar debe ser positiva.");
-        }
-    }
-
-    public bool RetirarDinero(decimal cantidad)
-    {
-        if (cantidad > 0 && Saldo >= cantidad)
-        {
-            Saldo -= cantidad;
-            Console.WriteLine($"Se han retirado ${cantidad} de la cuenta de {Titular.Nombre}");
-            return true;
-        }
-        else
-        {
-            Console.WriteLine("Fondos insuficientes o cantidad inválida.");
-            return false;
-        }
-    }
 }
