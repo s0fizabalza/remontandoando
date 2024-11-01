b16fe5a2394bfcef8f8c2d2e91013b43a5c01f2d
s0fizabalza <sofiromero108@gmail.com>
2024-11-01 00:37:57 -0300

    pepe2

--- ejer2/ejer2.cs
+++ ejer2/ejer2.cs
@@ -4,22 +4,11 @@ public class Persona
     public string Apellido { get; set; }
     public int Edad { get; set; }
 
+    // Constructor 
     public Persona(string nombre, string apellido, int edad)
     {
         Nombre = nombre;
         Apellido = apellido;
-        Edad = edad;   
-
-    }
-
-    public void Presentarse()
-    {
-        Console.WriteLine($"Hola, soy {Nombre} {Apellido}");
+        Edad = edad;
     }
 }
-
-
-Persona persona1 = new Persona("Juan", "Pérez", 30);
-
-
-persona1.Presentarse(); 
