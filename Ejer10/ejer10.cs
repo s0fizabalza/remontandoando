f5b5be84ed25b4b2ad0e9d7b8f5bd9f21007fb8d
s0fizabalza <sofiromero108@gmail.com>
2024-11-01 01:41:43 -0300

    ah no, este es pepe 10

---
+++ Ejer10/ejer10.cs
@@ -0,0 +1,21 @@
+public class Persona
+{
+    public const int MAYORIA_DE_EDAD = 18;
+
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int Edad { get; set; }
+
+    public Persona(string nombre, string apellido, int edad)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        Edad = edad;   
+
+    }
+
+     public bool EsMayorDeEdad()
+    {
+        return Edad >= MAYORIA_DE_EDAD;
+    }
+}
