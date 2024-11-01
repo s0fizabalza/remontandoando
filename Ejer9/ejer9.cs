3f9d171e2453303a57cedfd10a5ada55eaf252ea
s0fizabalza <sofiromero108@gmail.com>
2024-11-01 01:41:31 -0300

    pepe 10

---
+++ Ejer9/ejer9.cs
@@ -0,0 +1,22 @@
+public class Direccion
+{
+    public string Calle { get; set; }
+    public int Altura { get; set; }
+    public string Ciudad { get; set; }
+    public string Barrio { get; set; }
+
+    // Constructor
+    public Direccion(string calle, int altura, string ciudad, string barrio)
+    {
+        Calle = calle;
+        Altura = altura;
+        Ciudad = ciudad;
+        Barrio = barrio;
+    }
+
+
+public int ObtenerCodigoPostalFicticio()
+    {
+        return Altura * Calle.Length;
+    }
+}
